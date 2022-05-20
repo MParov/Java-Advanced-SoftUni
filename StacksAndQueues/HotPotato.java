922710130413208c16421d4aa5ebd3c00df2b655
MParov <mparov@gmail.com>
2022-05-20 22:21:37 +0300

    Stacks and Queues problems

---
+++ StacksAndQueues/HotPotato.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] players = scanner.nextLine().split("\\s+");
+        int round = Integer.valueOf(scanner.nextLine());
+        ArrayDeque<Integer> index = new ArrayDeque<>();
+        System.out.println();
+    }
+}
