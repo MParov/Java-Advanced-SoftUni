89408c8149e3b66434fcf1523e9a2dc5e161148c
MParov <mparov@gmail.com>
2022-05-22 13:12:32 +0300

    Stacks and Queues problems

--- StacksAndQueues/HotPotato.java
+++ StacksAndQueues/HotPotato.java
@@ -7,7 +7,21 @@ public class HotPotato {
 
         String[] players = scanner.nextLine().split("\\s+");
         int round = Integer.valueOf(scanner.nextLine());
-        ArrayDeque<Integer> index = new ArrayDeque<>();
-        System.out.println();
+        ArrayDeque<String> playersQueue = new ArrayDeque<>();
+
+        for (int i = 0; i < players.length; i++) {
+            playersQueue.offer(players[i]);
+        }
+        int counter = 1;
+        while (playersQueue.size() > 1) {
+            if (counter == round) {
+                System.out.println("Removed " + playersQueue.poll());
+                counter = 1;
+            }else{
+                counter++;
+                playersQueue.offer(playersQueue.poll());
+            }
+        }
+        System.out.println("Last is " + playersQueue.poll());
     }
 }
